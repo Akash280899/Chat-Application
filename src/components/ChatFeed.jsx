95dba9df90730c14dd8b65ae9e4d31f5ae2112e2
Akash280899 <janakiakash68@gmail.com>
2021-09-06 20:21:57 +0530

    mymessage and othersmessage setup

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -10,14 +10,48 @@ const ChatFeed = (props) => {
 
     const renderMessage = () => {
         const keys = Object.keys(messages);
-        console.log(keys);
+        
+        return keys.map((key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[index-1];
+            const isMymessage = userName === message.sender.username;
+             
+            return(
+                <div key={`msg_${index}`} style = {{width: '100%' }}>
+                    <div className="message-block">
+                        {
+                            isMymessage ? 
+                            <MyMessage message={message} /> : 
+                            <OthersMessage message={message} lastMessage={messages[lastMessageKey]}/>
+                        }
+                    </div>
+                    <div className="read-receipts" style={{marginRight : isMymessage ? '18px' : '0px' , marginLeft : isMymessage ? '0px' : '68px'}}>
+                        read-receipts
+                    </div>
+                </div>
+            )
+        })
     }
 
-    renderMessage();
+    if(!chat) return "loading..."
 
     return (
         <div>
-            ChatFeed
+            <div className="chat-feed">
+                <div className="chat-title-container">
+                    <div className="chat-title">
+                        {chat?.title}
+                    </div>
+                    <div className="chat-subtitle">
+                        {chat.people.map((person) => ` ${person.person.username}`)}
+                    </div>
+                </div>
+                {renderMessage()}
+                <div style={{height: '100px'}} />
+                <div className="message-form-container">
+                    <MessageForm {...props} chatId={activeChat} />
+                </div>
+            </div>
         </div>
     );
 }
